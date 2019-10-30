7fbf19a493eb16efb294b083c32ef604886ef345
Sunny Singh <sunny@localhost.localdomain>
2019-10-30 10:08:20 +0530

    Test bug solved for now

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_auth_flow/main.dart';
+// import 'package:flutter_auth_flow/main.dart';
+import '../lib/app/auth_flow.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(AuthFlow());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
